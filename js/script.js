09d07e305eb99a3ba04f8c9493dd144bb5368788
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-05-24 18:18:01 +0000

    made query search

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-04-kai-nguyen-4/sw.js", {
+    scope: "/ICS2O-Unit-6-04-kai-nguyen-4/",
+  })
+}
+
+window.onload = function() {
+  const params = new URLSearchParams(document.location.search)
+
+  const radius = params.get("radius")
+
+  const volume = 4/3 * Math.PI * (radius ** 3)
+  document.getElementById("radiusAnswer").innerHTML = radius + " mm<sup>3</sup> <br /><br />"
+  document.getElementById("answer").innerHTML = "the volume of the sphere is: " + volume
+}
+
